4c10e6d754d2ac15e9e4e81ee9cbef4ed2462a16
mattoh2410 <63155445+mattoh2410@users.noreply.github.com>
2020-08-05 21:53:01 +0100

    Update Lights.py

--- Lights.py
+++ Lights.py
@@ -6,10 +6,10 @@ class Lights:
     @staticmethod
     def set_lights(d, number):
         if d.jacket is not None and number == 1:
-            d.shorts.set_lights(cozmo.lights.red_light)
+            d.jacket.set_lights(cozmo.lights.red_light)
         if d.shorts is not None and number == 2:
-            d.shorts.set_lights(cozmo.lights.green_light)
+            d.shorts.set_lights(cozmo.lights.blue)
         if d.shorts is not None and number == 3:
-            d.sun_glasses.set_lights(cozmo.lights.blue_light)
+            d.shorts.set_lights(cozmo.lights.blue_light)
         if d.sun_glasses is not None and number == 3:
             d.sun_glasses.set_lights(cozmo.lights.green_light)
